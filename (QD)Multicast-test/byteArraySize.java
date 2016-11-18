9089d84bc2273e5b2764c0fefc8c42a8b23259ac
Hojeong Lee <spekum@nate.com>
2016-11-19 02:16:40 +0900

    deleted:    (QD)DateTest/DateTest0001/DateTest0001.java 	deleted:    (QD)DateTest/DateTest0002/DateTest0002.java 	deleted:    (QD)DateTest/DateTest0003/DateTest0003.java 	deleted:    (QD)DateTest/DateTest0004/DateTest0004.java 	deleted:    (QD)DateTest/DateTest0005/DateTest0005.java 	deleted:    (QD)DateTest/DateTest0005/DateTest005.java 	deleted:    (QD)DateTest/DateTest0006/DateTest0006.java 	deleted:    (QD)DateTest/DateTest0007/DateTest0007.java 	deleted:    (QD)DateTest/DateTest0008/DateTest0008.java 	deleted:    (QD)DateTest/DateTest0009/DateTest0009.java 	deleted:    (QD)DateTest/DateTest0010/DateTest0010.java 	deleted:    (QD)DateTest/DateTest0011/DateTest0011.java 	deleted:    (QD)DateTest/DateTest0012/DateTest0012.java 	deleted:    (QD)DayTime-MultiThtreaded-socket/DayTimeClient-Multithread-Socket/DayTimeClient.java 	deleted:    (QD)DayTime-MultiThtreaded-socket/DayTimeServer-Multithread-Socket/DayTimeServerThread.java 	deleted:    (QD)DayTime-MultiThtreaded-socket/DayTimeServer-Multithread-Socket/dtThread.java 	deleted:    (QD)Inheritance_Finalize_Test/Inheritance_Finalize_Test_001/src/shape_p.java 	deleted:    (QD)Inheritance_Finalize_Test/Inheritance_Finalize_Test_002/src/polygon_p_without_what.java 	deleted:    (QD)Inheritance_Finalize_Test/Inheritance_Finalize_Test_003/src/shape_p_without_finalize.java 	deleted:    (QD)Inheritance_Finalize_Test/Inheritance_Finalize_Test_004/src/polygon_p_without_finalize.java 	deleted:    (QD)MAC-Address-Display/MAC-Address-Display/src/MACAddr_Display.java 	deleted:    (QD)Multicast-test/0.Novice/RecvUDPMulticast.java 	deleted:    (QD)Multicast-test/0.Novice/SendUDPMulticast.java 	deleted:    (QD)Multicast-test/1.Original/RecvUDPMulticast.java 	deleted:    (QD)Multicast-test/1.Original/SendUDPMulticast.java 	deleted:    (QD)Multicast-test/byteArraySize.java 	deleted:    (QD)Multicast-test/dateStrPrn.java 	deleted:    (QD)Network-Interface-Information/Network_Interface_Information_Basic/ListNIFs.java 	deleted:    (QD)Network-Interface-Information/Network_Interface_Information_Ex/src/ListNetsEx.java 	deleted:    (QD)Network-Interface-Information/Network_Interface_Information_Simple/ListNets.java 	deleted:    (QD)Network-Interface-Information/TCPIP_Socket_In_Java_Chap01/InetAddressExampleJava2nd.java 	deleted:    (QD)TextEncoded/TextEncodedTextToBitOutput/Result of TextEncodedTextToBitOutput.txt.log 	deleted:    (QD)TextEncoded/TextEncodedTextToBitOutput/TextEncodedTextToBitOutput.java 	deleted:    (QD)TextEncoded/TextEncodedTextToBitOutput/bitsPrint.java 	deleted:    (QD)TextEncoded/TextEncodedTextToBitOutput/output-vista-32bit.txt.log 	deleted:    (QD)TextEncoded/TextEncodedTextToBitOutput/output-win7-64bit.txt.log 	deleted:    (QD)UDP-Message_Modification/UDPEchoClientTimeout/UDPEchoClientTimeout.java 	deleted:    (QD)UDP-Message_Modification/UDPEchoServer/UDPEchoServer.java 	modified:   .gitignore

--- (QD)Multicast-test/byteArraySize.java
+++
@@ -1,8 +0,0 @@
-public class byteArraySize {
-	public static void main(String args[]) {
-		String msg = "this is a Test" ;
-		byte[] byteMsg = msg.getBytes();
-		System.out.println("Size of byteMsg is " + byteMsg.length) ;
-		System.out.println("Size of msg is " + msg.length()) ;
-	}
-}
--- (QD)Multicast-test/dateStrPrn.java
+++
@@ -1,9 +0,0 @@
-import java.util.* ;
-
-public class dateStrPrn {
-	public static void main(String args[]) {
-//		Date date1 = new Date() ;
-//		System.out.println("date : " + date1.toString() );
-		System.out.println("date : " + (new Date()).toString() );
-	}
-}
